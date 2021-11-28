514d288e97ca088b323dcee60443695c7d7df642
Jayabharathy-R <94303082+Jayabharathy-R@users.noreply.github.com>
2021-11-28 12:39:54 +0530

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,14 @@
+// Print odd numbers in an array
+function odd(arr){
+    var oddArr=[];
+    for(i=0;i<arr.length;i++)
+    {
+        if(arr[i]%2!==0)
+        {
+            oddArr.push(arr[i]);
+        }
+    }
+    return oddArr;
+
+}
+console.log(odd([5,6,7,8,9,11,10]));
---
+++ sum.js
@@ -0,0 +1,12 @@
+// sum of numbers in an array
+function sum(arr){
+    var sum=0;
+    for(i=0;i<arr.length;i++)
+    {
+     sum=sum+arr[i];
+
+    }
+    return sum;
+
+}
+console.log(sum([1,2,3,4,5]));
